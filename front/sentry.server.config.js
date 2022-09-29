d5fd86000897d60fc230cdb0310d568f84bbd077
Павел Воробьев <pv@webpractik.ru>
2022-09-29 16:34:01 +0300

    slides

--- front/sentry.server.config.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  autoSessionTracking: false,
-  environment: `${process.env.APP_ENV}-server`,
-});
-
--- front/src/components/NewPaginatedGrid/queries.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getProducts } from '@/pages';
-
-export const useProducts = (page: number, searchString: string) => {
-    return useQuery(['products', searchString, page], () => getProducts(page, searchString), {
-        keepPreviousData: true,
-        staleTime: 5000,
-    });
-};
